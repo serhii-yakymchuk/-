c1ae34b309397bdc7af5067926c8e0870e462cbb
serhii-yakymchuk <serhii.yakymchuk.dev@gmail.com>
2026-01-20 22:16:21 +0200

    Update script.js
    refactor: clean up code

--- script.js
+++ script.js
@@ -14,29 +14,22 @@ async function getRate() {
     coef = toRate / fromRate; 
     console.log(`Курс ${selectedInp} до ${selectedRes}:`, coef);
     
-    // Оновлюємо результат відразу після отримання курсу
-    renderResult(); 
+    renderResult(); // Оновлюємо результат відразу після отримання курсу
 }
-
 function updateSelectInp() { 
     selectedInp = document.getElementById('valutaInp').value; 
     getRate(); // Коли змінили валюту — тягнемо новий курс
 }
-
 function updateSelectRes() { 
     selectedRes = document.getElementById('valutaRes').value; 
     getRate(); // Коли змінили валюту — тягнемо новий курс
 }
-
 function renderResult() { 
     myData = document.getElementById('num1').value;
     let finalResult = (myData * coef).toFixed(2);
     document.getElementById('result').textContent = finalResult;
 }
-
 function showNum1() { 
     renderResult(); // Просто рахуємо на льоту, не смикаючи API без зупинки
 }
-
-// Викличемо один раз при завантаженні, щоб мати початковий курс
-getRate();
+getRate(); // Викличемо один раз при завантаженні, щоб мати початковий курс
